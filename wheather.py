c674bc96aad5b82cca1a9953dbdd4cb3cd6b74a1
jose <joselikidom@gmail.com>
2014-02-07 12:01:43 +0100

    Update wheather.py

--- wheather.py
+++ wheather.py
@@ -15,5 +15,5 @@ temp1 = int(raw_input("de que ciudad quieres saber la temperatura actual?"))
 req = requests.get("http://api.openweathermap.org/data/2.5/weather", params={'q':'%s,spain'%provincias[temp1]})
 dato = json.loads(req.text)
 temperatura = dato['main']['temp']-273
-print "la temperatura actual de",provincias[temp1], "es:",temperatura
+print "la temperatura actual de",provincias[temp1], "es:",temperatura, "grados centigrados"
 
